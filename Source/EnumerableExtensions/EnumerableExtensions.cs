9410884ad4dcdef1ddaedd3951dcfc69e3b3ea5b
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-11-20 11:53:35 +0100

    Added `WithIndex` `IEnumerable<T>` extension method

---
+++ Source/EnumerableExtensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Common.EnumerableExtensions;
+
+public static class EnumerableExtensions
+{
+	/// <summary>
+	/// Projects each element of a sequence into a tuple of that item with its index.
+	/// </summary>
+	/// <param name="source">A sequence of values to invoke a transform function on.</param>
+	/// <typeparam name="T">The type of items in source sequence.</typeparam>
+	public static IEnumerable<(T Item, int Index)> WithIndex<T>(this IEnumerable<T> source)
+	{
+		return source.Select((item, index) => (item, index));
+	}
+}

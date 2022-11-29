e957dbdb93e54f82177ea4baeb878ad9d877fc75
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-11-29 15:24:34 +0100

    Added StringExtensions docstrings

--- Source/Extensions/StringExtensions.cs
+++ Source/Extensions/StringExtensions.cs
@@ -3,9 +3,20 @@ using System.IO;
 
 namespace AdventOfCode.Common.StringExtensions;
 
+/// <summary>
+/// Extension methods for <see cref="string"/>.
+/// </summary>
 public static class StringExtensions
 {
 
+	/// <summary>
+	/// Enumerates the lines of the string.
+	/// </summary>
+	/// <param name="str">The string the enumerator will work on.</param>
+	/// <remarks>
+	/// The enumerable is agnostic of line endings, that is it will work with any
+	/// as does <see cref="StringReader.ReadLine"/>.
+	/// </remarks>
 	public static IEnumerable<string> EnumerateLines(this string str)
 	{
 		StringReader reader = new(str);
